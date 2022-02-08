68f2d611b312539e052af0a34d1559a93bb21515
kilicEzgi <99246213+kilicEzgi@users.noreply.github.com>
2022-02-08 12:07:48 +0300

    Add files via upload

---
+++ WindowsFormsApplication28/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication28
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "ezgı adamdır ezo pıc kubrada cakma prenses serkanda gercek premses";
+            MessageBox.Show("ezo pıctır dalınn");
+        }
+    }
+}
